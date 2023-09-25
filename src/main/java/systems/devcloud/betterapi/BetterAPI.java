bbc115412257460b76b8ee7e9ff767baddfdf970
Sebastian Schindler <sebastian@schindlertai.de>
2023-09-25 21:12:51 +0200

    :sparkles: feat: Initialize Repository
    :sparkles: feat[translation]; Implemented Localizer

---
+++ src/main/java/systems/devcloud/betterapi/BetterAPI.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2023 DevCloud Software. All Rights reserved.
+ ******************************************************************************/
+package systems.devcloud.betterapi;
+
+import lombok.extern.java.Log;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.Configuration;
+import org.bukkit.plugin.java.JavaPlugin;
+import systems.devcloud.betterapi.utils.Localizer;
+
+/**
+ * Main Class of the BetterAPI Plugin.
+ */
+@Log(topic = "BetterAPI")
+public final class BetterAPI extends JavaPlugin {
+
+    public static Localizer localizer;
+    private int apiPort;
+
+    @Override
+    public void onLoad() {
+        Configuration pluginConfig = getConfig();
+        localizer = new Localizer(pluginConfig.getString("general.locale"));
+        this.apiPort = pluginConfig.getInt("api.port");
+        log.info(String.format(localizer.get("plugin.loaded"), localizer.get("prefix")));
+    }
+
+    /**
+     * Called when the plugin is enabled.
+     */
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        log.info(String.format(localizer.get("plugin.enabled"), localizer.get("prefix")));
+    }
+
+    @Override
+    public void onDisable() {
+        log.info(String.format(localizer.get("plugin.disabled"), localizer.get("prefix")));
+    }
+}
---
+++ src/main/java/systems/devcloud/betterapi/utils/Localizer.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2023 DevCloud Software. All Rights reserved.
+ ******************************************************************************/
+
+package systems.devcloud.betterapi.utils;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+import lombok.extern.java.Log;
+
+@Log (topic = "BetterAPI")
+public class Localizer {
+
+    private final ResourceBundle language;
+
+    public Localizer(String configuredLanguage) {
+        String[] langCode = langSplitter(configuredLanguage);
+        Locale locale = new Locale(langCode[0], langCode[1]);
+        this.language = ResourceBundle.getBundle("languages/language", locale);
+        log.info(get("plugin.localizer.loaded"));
+    }
+
+    public String get(String key) {
+        return language.getString(key);
+    }
+
+    private String[] langSplitter(String lang) {
+        return lang.split("_");
+    }
+}
